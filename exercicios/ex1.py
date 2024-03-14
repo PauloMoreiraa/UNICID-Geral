8dfd547a645075c128bf3e0a4dc0cf8be5bd30cf
Paulo Moreira <56117238+PauloMoreiraa@users.noreply.github.com>
2024-03-14 10:42:52 -0300

    só pra deixar salvo

---
+++ exercicios/ex1.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite a primeira nota \n"))
+n2 = int(input("Digite a segunda nota \n"))
+n3 = int(input("Digite a terceira nota \n"))
+n4 = int(input("Digite a quarta nota \n"))
+
+media = (n1+n2+n3+n4)/4
+
+print('A média é: ')
+print(media)
